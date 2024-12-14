714f01ee3157cf0cabe188b6d6e1f3db6cfcaff0
nimaaskarian <nimaaskarian.work@outlook.com>
2024-12-14 09:48:07 +0330

    Add portion selection

--- main.py
+++ main.py
@@ -24,23 +24,47 @@ def main():
             name, url = name_url
             writer = csv.writer(args.foodsfile)
             csv_dict_reader = csv.DictReader(args.foodsfile)
-            row = {key: value for key, value, _ in take(len(keys), get_keys(keys, url, units))}
-            keys.insert(0, "Name")
+            portions, select = get_portions_element(url)
+            portions = list(portions)
+            if len(portions) > 1:
+                portion = set_portion_fzf(portions, select)
+            else:
+                portion = portions[0]
+            row = {key: value for key, value, _ in take(len(keys), get_keys(keys, units))}
             row["Name"] = name
+            row["Portion"] = portion
             for key in keys:
                 if not (item:=row.get(key,0)):
                     row[key] = item
-            writer = csv.DictWriter(args.foodsfile, keys)
+            writer = csv.DictWriter(args.foodsfile, ["Name", "Portion"]+keys, lineterminator="\n")
             if not any(csv_dict_reader):
                 writer.writeheader()
             writer.writerow(row)
             args.foodsfile.close()
 
-def get_keys(keys, url, units={}):
+def set_portion_fzf(portions, select):
+    if i:=iterator_fzf_select(portions):
+        select.select_by_visible_text(portions[i])
+        return portions[i]
+
+def get_portions_element(url):
     from selenium.webdriver.support import expected_conditions as EC
     from selenium.webdriver.support.wait import WebDriverWait
-    driver.get(url)
     from selenium.webdriver.common.by import By
+    from selenium.webdriver.support.select import Select
+    driver.get(url)
+    selector = By.ID, "nutrient-per-selection-Survey-or-branded"
+    element = WebDriverWait(driver, 2).until(EC.presence_of_element_located(selector))
+    select = Select(element)
+    return (item.text for item in select.options), select
+    # for item in select.options: yield item.text
+
+
+def get_keys(keys, units={}):
+    from selenium.webdriver.support import expected_conditions as EC
+    from selenium.webdriver.support.wait import WebDriverWait
+    from selenium.webdriver.common.by import By
+    WebDriverWait(driver, 5).until(EC.invisibility_of_element((By.ID, "floatingCirclesG")))
     selector = By.CSS_SELECTOR, "app-food-nutrients>div>div>table>tbody>tr"
     WebDriverWait(driver, 2).until(EC.presence_of_element_located(selector))
     for item in driver.find_elements(*selector):
@@ -56,6 +80,13 @@ def get_keys(keys, url, units={}):
             yield key, float(value), unit
 
 def prompt_url_fzf(query):
+    to_str = lambda i, name_url_cat: f"{i}. \"{name_url_cat[0]}\" in \"{name_url_cat[2]}\"\n"
+    name_urls = []
+    callback = lambda _, name_url: name_urls.append((name_url[0], name_url[1]))
+    if i:=iterator_fzf_select(search(query), callback, to_str):
+        return name_urls[i]
+
+def iterator_fzf_select(iterator, callback=None, to_str=lambda i, item: f"{i}. {item}\n"):
     import subprocess
     process = subprocess.Popen(
             "fzf",
@@ -66,19 +97,19 @@ def prompt_url_fzf(query):
         )
     if process.stdin is None or process.stdout is None:
         return
-    urls = []
     try:
-        for i, (name, url, category) in enumerate(search(query)):
-            process.stdin.write(f"{i}. \"{name}\" in \"{category}\"\n")
+        for i, item in enumerate(iterator):
+            if process.poll():
+                break
+            process.stdin.write(to_str(i, item))
             process.stdin.flush()
-            urls.append((name,url))
+            if callback is not None:
+                callback(i, item)
         process.stdin.close()
     except BrokenPipeError:
         pass
     if output:=process.stdout.read():
-        index = int(output[:output.index(".")])
-        return urls[index]
-    return None
+        return int(output[:output.index(".")])
 
 def search(query):
     from selenium.webdriver.support import expected_conditions as EC
